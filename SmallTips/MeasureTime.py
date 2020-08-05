8d3674e0e42c474045cdc7d0f0d43e2d0cb966ca
Daisuke Akagawa <daisuke1024akagawa@gmail.com>
2020-08-05 11:33:45 +0900

    Update MeasureTime.py

--- SmallTips/MeasureTime.py
+++ SmallTips/MeasureTime.py
@@ -1,18 +1,27 @@
 import time
 
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        st = time.time()
-        func(*args, **kwargs)
-        print(f"Process time : {time.time() - st}")
-       
-    return wrapper
+def timer(*dargs, **dkwargs):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            st = time.time()
+            iteration = dkwargs["iteration"]
+            
+            for _ in range(iteration):
+                func(*args, **kwargs)
+            
+            avg_time = (time.time() - st) / iteration
+            print(f"Average Time: {avg_time}   (iteration: {iteration})")
+    
+        return wrapper
+    
+    return decorator
     
     
 if __name__ == "__main__":
-    @timer
+    @timer(iteration=100)
     def timer_test():
-        print("Timer test function")
+        time.sleep(0.01)
     
     timer_test()
+    # >>> Average Time: 0.0118611478805542   (iteration: 100)
