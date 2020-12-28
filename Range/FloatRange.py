179ecba2c5071dfdf64cfad02b2ce1d47469a732
Daisuke Akagawa <daisuke1024akagawa@gmail.com>
2020-12-28 14:19:04 +0900

    Update FloatRange.py

--- Range/FloatRange.py
+++ Range/FloatRange.py
@@ -3,8 +3,8 @@ def float_range(min_: float, max_: float, step_size: int,
     """ iterator for providing range that data-type is float
 
     Arguments:
-        _min {float} -- minimum boundary value
-        _max {float} -- maximum boundary value
+        min_ {float} -- minimum boundary value
+        max_ {float} -- maximum boundary value
         step_size {int} -- how much you want to step
         round_pos {int} -- which position to round error (default: 5)
         is_ascending {bool} -- if you want to sort in ascending order, set this as True
@@ -12,12 +12,12 @@ def float_range(min_: float, max_: float, step_size: int,
     """
     assert not step_size == 0, "Please specified step_size over 0"
 
-    step_num = int((_max - _min) / step_size)
+    step_num = int((max_ - min_) / step_size)
 
     if is_ascending:
-        value = [round(_min + step_size * i, round_pos) for i in range(step_num)]
+        value = [round(min_ + step_size * i, round_pos) for i in range(step_num)]
     else:
-        value = [round(_max - step_size * i, round_pos) for i in range(step_num)]
+        value = [round(max_ - step_size * i, round_pos) for i in range(step_num)]
 
     for v in value:
         yield v
